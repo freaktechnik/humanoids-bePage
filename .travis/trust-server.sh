1e565c5401e62ebfdefeede78eebc9102a3a54b6
Martin Giger <martin@humanoids.be>
2019-04-13 01:27:34 +0200

    Move deploy_rsa to a safer place

--- .travis/trust-server.sh
+++ .travis/trust-server.sh
@@ -1,6 +1,6 @@
 #! /bin/bash
 eval "$(ssh-agent -s)"
-openssl aes-256-cbc -K $encrypted_f65e690cb8c7_key -iv $encrypted_f65e690cb8c7_iv -in $TRAVIS_BUILD_DIR/.travis/deploy_rsa.enc -out $TRAVIS_BUILD_DIR/.travis/deploy_rsa -did_rsa -d
-chmod 600 $TRAVIS_BUILD_DIR/.travis/deploy_rsa
-ssh-add $TRAVIS_BUILD_DIR/.travis/deploy_rsa
+openssl aes-256-cbc -K $encrypted_f65e690cb8c7_key -iv $encrypted_f65e690cb8c7_iv -in $TRAVIS_BUILD_DIR/.travis/deploy_rsa.enc -out /tmp/deploy_rsa -did_rsa -d
+chmod 600 /tmp/deploy_rsa
+ssh-add /tmp/deploy_rsa
 echo "$DEPLOY_HOST $SERVER_PUBLIC_KEY" >> $HOME/.ssh/known_hosts
