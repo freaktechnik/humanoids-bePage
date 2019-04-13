c4924c7dfa0c28337ceaf8e91f1eef29660f5de0
Martin Giger <martin@humanoids.be>
2019-04-13 20:06:25 +0200

    Maybe now this is properly encrypted an all that?

--- .travis/trust-server.sh
+++ .travis/trust-server.sh
@@ -1,6 +1,5 @@
 #! /bin/bash
 eval "$(ssh-agent -s)"
-openssl aes-256-cbc -K $encrypted_f65e690cb8c7_key -iv $encrypted_f65e690cb8c7_iv -in .travis/deploy_rsa.enc -out .travis/deploy_rsa -d
 chmod 600 .travis/deploy_rsa
 ssh-add .travis/deploy_rsa
 echo "$DEPLOY_HOST $SERVER_PUBLIC_KEY" >> $HOME/.ssh/known_hosts
