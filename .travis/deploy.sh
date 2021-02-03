1e152a6ae70f25efbd2567333db0916fb06dd9f3
Martin Giger <martin@humanoids.be>
2021-02-03 20:27:06 +0100

    feat: github actions, here I come

--- .travis/deploy.sh
+++
@@ -1,5 +0,0 @@
-#! /bin/bash
-set -x
-source $TRAVIS_BUILD_DIR/.travis/push.sh
-source $TRAVIS_BUILD_DIR/.travis/trust-server.sh
-deploy "hbp"
--- .travis/trust-server.sh
+++
@@ -1,5 +0,0 @@
-#! /bin/bash
-eval "$(ssh-agent -s)"
-chmod 600 .travis/deploy_rsa
-ssh-add .travis/deploy_rsa
-echo "$DEPLOY_HOST $SERVER_PUBLIC_KEY" >> $HOME/.ssh/known_hosts
